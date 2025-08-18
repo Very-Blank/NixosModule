44d7ce291e9c710565398e1216c2eb9ffda04ef7
very-blank <aapeli.saarelainen.76@gmail.com>
2025-08-18 20:18:52 +0300

    feat: Changes

--- modules/tty/greetd/default.nix
+++ modules/tty/greetd/default.nix
@@ -3,9 +3,14 @@
     services.greetd = {
       enable = true;
       settings = {
+        terminal = {
+          vt = 1;
+        };
+        
+        # TODO: MAKE THIS BUETIFULLLLL and not autoenable
         default_session = {
-          command = "${pkgs.niri}/bin/niri-session";
-          user = config.modules.home.user.name;
+          command = "${pkgs.tuigreet}/bin/tuigreet --time --cmd ${pkgs.niri}/bin/niri-session";
+          user = "greeter";
         };
       };
     };
