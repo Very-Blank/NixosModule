3b0133747c1fe4820c04461c0a3d22426c649a87
very-blank <aapeli.saarelainen.76@gmail.com>
2025-08-21 10:29:35 +0300

    fix: Looks ungly

--- modules/tty/greetd/default.nix
+++ modules/tty/greetd/default.nix
@@ -18,8 +18,8 @@
   config = lib.mkIf config.modules.tty.greetd.enable {
     services = {
       getty = {
-        greetingLine = " << NixOS ${config.system.nixos.release} >>\n";
-        helpLine = lib.mkForce " ${(lib.strings.toUpper (builtins.substring 0 1 config.modules.networking.hostname))}${(builtins.substring 1 (builtins.stringLength config.modules.networking.hostname) config.modules.networking.hostname)} at your service.";
+        greetingLine = "<< NixOS ${config.system.nixos.release} >>\n";
+        helpLine = lib.mkForce "${(lib.strings.toUpper (builtins.substring 0 1 config.modules.networking.hostname))}${(builtins.substring 1 (builtins.stringLength config.modules.networking.hostname) config.modules.networking.hostname)} at your service.";
       };
 
       greetd = {
