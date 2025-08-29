4e54d4b40805942cc467d06fa7ed293fcb7eae84
very-blank <aapeli.saarelainen.76@gmail.com>
2025-08-29 12:41:46 +0300

    fix: Suppressing warnings that come up on login.

--- modules/boot/default.nix
+++ modules/boot/default.nix
@@ -9,21 +9,22 @@
     };
   };
 
-  config = lib.mkMerge [
+  config = lib.mkMerge [ lib.mkMerge [
     (lib.mkIf config.modules.boot.multiboot.enable {
-      boot.loader.systemd-boot.enable = false;
-
       boot.loader.grub.enable = true;
       boot.loader.grub.device = "nodev";
       boot.loader.grub.useOSProber = true;
       boot.loader.grub.efiSupport = true;
 
       boot.loader.efi.efiSysMountPoint = "/boot";
-      boot.loader.efi.canTouchEfiVariables = true;
+
     })
     (lib.mkIf (!config.modules.boot.multiboot.enable) {
       boot.loader.systemd-boot.enable = true;
+    })]
+    {
+      boot.consoleLogLevel = 3;
       boot.loader.efi.canTouchEfiVariables = true;
-    })
+    }
   ];
 }
