1479c4d2d0cc0042fd3011d5e490c16f9d47a3dd
very-blank <aapeli.saarelainen.76@gmail.com>
2025-10-04 12:31:31 +0300

    closes #2

--- modules/graphical/firefox/default.nix
+++ modules/graphical/firefox/default.nix
@@ -1,4 +1,10 @@
-{lib, config, pkgs, ...}: {
+{
+  lib,
+  config,
+  pkgs,
+  ...
+}:
+{
   options = {
     modules = {
       graphical = {
@@ -25,7 +31,7 @@
           DisableTelemetry = true;
           DisableFirefoxStudies = true;
           EnableTrackingProtection = {
-            Value= true;
+            Value = true;
             Locked = true;
             Cryptomining = true;
             Fingerprinting = true;
@@ -62,38 +68,55 @@
             };
           };
 
-          Preferences = let
-            lock-false = {
-              Value = false;
-              Status = "locked";
+          Preferences =
+            let
+              lock-false = {
+                Value = false;
+                Status = "locked";
+              };
+              lock-true = {
+                Value = true;
+                Status = "locked";
+              };
+            in
+            {
+              "browser.contentblocking.category" = {
+                Value = "strict";
+                Status = "locked";
+              };
+              "extensions.pocket.enabled" = lock-false;
+              "extensions.screenshots.disabled" = lock-true;
+              "browser.topsites.contile.enabled" = lock-false;
+              "browser.formfill.enable" = lock-false;
+              "browser.search.suggest.enabled" = lock-false;
+              "browser.search.suggest.enabled.private" = lock-false;
+              "browser.urlbar.suggest.searches" = lock-false;
+              "browser.urlbar.showSearchSuggestionsFirst" = lock-false;
+              "sidebar.main.tools" = {
+                Value = "bookmarks";
+                Status = "locked";
+              };
+              "sidebar.verticalTabs" = lock-true;
+              "browser.layout.showSideBar" = lock-true;
+              "browser.newtabpage.activity-stream.feeds.section.topstories" = lock-false;
+              "browser.newtabpage.activity-stream.feeds.snippets" = lock-false;
+              "browser.newtabpage.activity-stream.section.highlights.includePocket" = lock-false;
+              "browser.newtabpage.activity-stream.section.highlights.includeBookmarks" = lock-false;
+              "browser.newtabpage.activity-stream.section.highlights.includeDownloads" = lock-false;
+              "browser.newtabpage.activity-stream.section.highlights.includeVisited" = lock-false;
+              "browser.newtabpage.activity-stream.showSponsored" = lock-false;
+              "browser.newtabpage.activity-stream.system.showSponsored" = lock-false;
+              "browser.newtabpage.activity-stream.showSponsoredTopSites" = lock-false;
+
+              "signon.rememberSignons" = lock-false;
+              "signon.autofillForms" = lock-false;
+              "signon.autofillForms.autocompleteOff" = lock-false;
+              "signon.generation.enabled" = lock-false;
+              "signon.generation.available" = lock-false;
+              "signon.management.page.breach-alerts.enabled" = lock-false;
+              "signon.management.page.vulnerable-passwords.enabled" = lock-false;
+              "signon.backup.enabled" = lock-false;
             };
-            lock-true = {
-              Value = true;
-              Status = "locked";
-            };
-              in {
-            "browser.contentblocking.category" = { Value = "strict"; Status = "locked"; };
-            "extensions.pocket.enabled" = lock-false;
-            "extensions.screenshots.disabled" = lock-true;
-            "browser.topsites.contile.enabled" = lock-false;
-            "browser.formfill.enable" = lock-false;
-            "browser.search.suggest.enabled" = lock-false;
-            "browser.search.suggest.enabled.private" = lock-false;
-            "browser.urlbar.suggest.searches" = lock-false;
-            "browser.urlbar.showSearchSuggestionsFirst" = lock-false;
-            "sidebar.main.tools" = { Value = "bookmarks"; Status = "locked"; };
-            "sidebar.verticalTabs" = lock-true;
-            "browser.layout.showSideBar" = lock-true;
-            "browser.newtabpage.activity-stream.feeds.section.topstories" = lock-false;
-            "browser.newtabpage.activity-stream.feeds.snippets" = lock-false;
-            "browser.newtabpage.activity-stream.section.highlights.includePocket" = lock-false;
-            "browser.newtabpage.activity-stream.section.highlights.includeBookmarks" = lock-false;
-            "browser.newtabpage.activity-stream.section.highlights.includeDownloads" = lock-false;
-            "browser.newtabpage.activity-stream.section.highlights.includeVisited" = lock-false;
-            "browser.newtabpage.activity-stream.showSponsored" = lock-false;
-            "browser.newtabpage.activity-stream.system.showSponsored" = lock-false;
-            "browser.newtabpage.activity-stream.showSponsoredTopSites" = lock-false;
-          };
         };
       };
     };
