19c3045c2ee6f78c432de9dbb0e611d42233951c
very-blank <aapeli.saarelainen.76@gmail.com>
2025-11-19 18:54:09 +0200

    fix: Wrong lsp.

--- modules/developer/haskell/default.nix
+++ modules/developer/haskell/default.nix
@@ -19,7 +19,7 @@
   config = lib.mkIf config.modules.developer.haskell.enable {
     userHome = {
       home.packages = [
-        pkgs.haskellPackages.lsp
+        pkgs.haskellPackages.lsp-types
         pkgs.stack
       ];
     };
