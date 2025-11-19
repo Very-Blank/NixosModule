a8be79ef09896870d9b218ba69463b13d7158d68
very-blank <aapeli.saarelainen.76@gmail.com>
2025-11-19 19:08:37 +0200

    fix: Cabal?

--- modules/developer/haskell/default.nix
+++ modules/developer/haskell/default.nix
@@ -19,7 +19,6 @@
   config = lib.mkIf config.modules.developer.haskell.enable {
     userHome = {
       home.packages = [
-        pkgs.haskellPackages.Cabal
         pkgs.ghc
         pkgs.haskell-language-server
         pkgs.stack
