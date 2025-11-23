95551e23aaa7868d734ef9db37d40252a2252cb4
very-blank <aapeli.saarelainen.76@gmail.com>
2025-11-23 21:48:29 +0200

    fix: Changes.

--- modules/terminal/tmux/default.nix
+++ modules/terminal/tmux/default.nix
@@ -23,7 +23,7 @@
         keyMode = "vi";
         # FUCK YOU, https://github.com/nix-community/home-manager/issues/5952
         extraConfig = ''
-          set -g default-command ${pkgs.zsh}/bin/zsh
+          set-option -g default-shell ${pkgs.zsh}/bin/zsh
           set-option -g prefix C-a
           unbind C-b
           bind C-a send-prefix
