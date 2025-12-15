19b9633e2a97e226e45e049fba4a034b6109a449
very-blank <aapeli.saarelainen.76@gmail.com>
2025-12-15 16:16:25 +0200

    feat: Steam.

---
+++ modules/graphical/applications/other/steam/default.nix
@@ -0,0 +1,23 @@
+{
+  config,
+  mkIfModule,
+  ...
+}:
+mkIfModule config [ "graphical" "applications" "other" "steam" ] {
+  config = {
+    modules.unfreePackages = [
+      "steam"
+      "steam-original"
+      "steam-unwrapped"
+      "steam-run"
+    ];
+
+    userHome = {
+      programs.steam = {
+        enable = true;
+        dedicatedServer.openFirewall = true;
+        localNetworkGameTransfers.openFirewall = true;
+      };
+    };
+  };
+}
