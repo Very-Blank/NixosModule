578a6eeff6ddcb604754fc2a1618d3a54fa8715c
Very-Blank <aapeli.saarelainen.76@gmail.com>
2025-12-20 09:53:07 +0200

    feat: Server up?

--- hosts/ouroboros/default.nix
+++ hosts/ouroboros/default.nix
@@ -46,12 +46,13 @@
 
       server = {
         openssh = {
+          enable = true;
           keys = [
             "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGU4pH0jqPg+3y2k0OqKhFZyw5MvQ7Z3y9A85QK2cNPu hermes"
           ];
         };
 
-        openssh.enable = true;
+        ddclient.enable = true;
       };
 
       graphical = {
