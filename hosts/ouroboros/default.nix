49f33fa3c82acef2c34fdbb549a5b41acb8adbbc
very-blank <aapeli.saarelainen.76@gmail.com>
2025-12-20 06:35:01 +0200

    feat: Changes.

--- hosts/ouroboros/default.nix
+++ hosts/ouroboros/default.nix
@@ -26,6 +26,12 @@
       };
 
       server = {
+        openssh = {
+          keys = [
+            "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGU4pH0jqPg+3y2k0OqKhFZyw5MvQ7Z3y9A85QK2cNPu hermes"
+          ];
+        };
+
         openssh.enable = true;
       };
 
