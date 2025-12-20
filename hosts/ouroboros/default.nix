f14bebbf05b086f8e4a6a31998d0537bb61c9578
Very-Blank <aapeli.saarelainen.76@gmail.com>
2025-12-20 08:35:59 +0200

    fix: Disable sleep.

--- hosts/ouroboros/default.nix
+++ hosts/ouroboros/default.nix
@@ -9,7 +9,10 @@
     hostname = "ouroboros";
 
     services.upower.ignoreLid = true;
-    systemd.sleep.extraConfig = "AllowHibernation=no";
+    systemd.targets.sleep.enable = false;
+    systemd.targets.suspend.enable = false;
+    systemd.targets.hibernate.enable = false;
+    systemd.targets.hybrid-sleep.enable = false;
 
     modules = {
       terminal = {
