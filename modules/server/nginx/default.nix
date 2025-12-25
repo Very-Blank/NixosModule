37e82f53eaf9e3153258b04dd3ec57639fafd2a5
very-blank <aapeli.saarelainen.76@gmail.com>
2025-12-25 15:04:02 +0200

    feat: Change formatting.

--- modules/server/nginx/default.nix
+++ modules/server/nginx/default.nix
@@ -5,36 +5,36 @@
   ...
 }:
 mkIfModule config
-  [
-    "server"
-    "nginx"
-  ]
-  {
-    options = {
-      acme = {
-        email = lib.mkOption {
-          description = "Email address";
-          type = lib.types.nonEmptyStr;
-        };
+[
+  "server"
+  "nginx"
+]
+{
+  options = {
+    acme = {
+      email = lib.mkOption {
+        description = "Email address";
+        type = lib.types.nonEmptyStr;
+      };
+    };
+  };
+
+  config = cfg: {
+    security.acme = {
+      acceptTerms = true;
+      defaults.email = cfg.acme.email;
+    };
+
+    services.nginx = {
+      enable = true;
+
+      virtualHosts.${config.modules.server.domain.main} = {
+        forceSSL = true;
+        enableACME = true;
+        reuseport = true;
       };
     };
 
-    config = cfg: {
-      security.acme = {
-        acceptTerms = true;
-        defaults.email = cfg.acme.email;
-      };
-
-      services.nginx = {
-        enable = true;
-
-        virtualHosts.${config.modules.server.domain.main} = {
-          forceSSL = true;
-          enableACME = true;
-          reuseport = true;
-        };
-      };
-
-      users.users."nginx".extraGroups = [ "acme" ];
-    };
-  }
+    users.users."nginx".extraGroups = ["acme"];
+  };
+}
