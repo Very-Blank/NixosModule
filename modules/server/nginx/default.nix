d86f7fdbc2e203bceffb31f2a9b8d8f2d125e38f
Very-Blank <aapeli.saarelainen.76@gmail.com>
2025-12-27 21:32:45 +0200

    feat: Changes.

--- modules/server/nginx/default.nix
+++ modules/server/nginx/default.nix
@@ -34,22 +34,16 @@ mkIfModule config
       recommendedProxySettings = true;
       recommendedTlsSettings = true;
 
-      # appendHttpConfig = ''
-      #   # Enable CSP for your services.
-      #   #add_header Content-Security-Policy "script-src 'self'; object-src 'none'; base-uri 'none';" always;
-      #
-      #   # Minimize information leaked to other domains
-      #   add_header 'Referrer-Policy' 'origin-when-cross-origin';
-      #
-      #   # Disable embedding as a frame
-      #   add_header X-Frame-Options DENY;
-      #
-      #   # Prevent injection of code in other mime types (XSS Attacks)
-      #   add_header X-Content-Type-Options nosniff;
-      #
-      #   # This might create errors
-      #   proxy_cookie_path / "/; secure; HttpOnly; SameSite=strict";
-      # '';
+      appendHttpConfig = ''
+        # Minimize information leaked to other domains
+        add_header 'Referrer-Policy' 'origin-when-cross-origin';
+
+        # Disable embedding as a frame
+        add_header X-Frame-Options DENY;
+
+        # Prevent injection of code in other mime types (XSS Attacks)
+        add_header X-Content-Type-Options nosniff;
+      '';
 
       sslCiphers = "AES256+EECDH:AES256+EDH:!aNULL";
 
