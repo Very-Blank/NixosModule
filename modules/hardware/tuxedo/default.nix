aab0557fbfb35dd3626bd80a2052e7aaa68af74c
very-blank <aapeli.saarelainen.76@gmail.com>
2026-01-17 12:13:17 +0200

    feat: Update.

--- modules/hardware/tuxedo/default.nix
+++ modules/hardware/tuxedo/default.nix
@@ -18,12 +18,15 @@
   in
     lib.mkIf cfg.enable {
       hardware = {
-        tuxedo-drivers.enable = true;
+        # FIXME: Waiting for: <https://github.com/NixOS/nixpkgs/pull/479580>
+        #        For now it's disabled.
 
-        tuxedo-rs = {
-          enable = true;
-          tailor-gui.enable = true;
-        };
+        # tuxedo-drivers.enable = true;
+
+        # tuxedo-rs = {
+        #   enable = true;
+        #   tailor-gui.enable = true;
+        # };
       };
     };
 }
