1451733e9f7f88cab95043e8b25c7dcee80227d2
very-blank <aapeli.saarelainen.76@gmail.com>
2026-02-27 20:40:49 +0200

    feat: Version upgrade for nextcloud.

--- modules/server/nextcloud/default.nix
+++ modules/server/nextcloud/default.nix
@@ -23,7 +23,7 @@
 
       services.nextcloud = {
         enable = true;
-        package = pkgs.nextcloud32;
+        package = pkgs.nextcloud33;
 
         hostName = "${subdomainName}.${config.modules.server.domain.main}";
         https = true;
