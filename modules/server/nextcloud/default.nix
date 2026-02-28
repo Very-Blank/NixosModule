3eca636579abe47f353422b6584ea5fe2fac8dc8
very-blank <aapeli.saarelainen.76@gmail.com>
2026-02-28 13:56:55 +0200

    fix: Remove html entities.

--- modules/server/nextcloud/default.nix
+++ modules/server/nextcloud/default.nix
@@ -49,40 +49,37 @@
       #     };
       #   };
       # };
-      #
-      # systemd.services."nextcloud-setup".requires = ["postgresql.service"];
-      # systemd.services."nextcloud-setup".after = ["postgresql.service"];
-      #
-      # services.fail2ban = {
-      #   enable = true;
-      #   # The jail file defines how to handle the failed authentication attempts found by the Nextcloud filter
-      #   # Ref: https://docs.nextcloud.com/server/latest/admin_manual/installation/harden_server.html#setup-a-filter-and-a-jail-for-nextcloud
-      #   jails = {
-      #     nextcloud.settings = {
-      #       # START modification to work with syslog instead of logile
-      #       backend = "systemd";
-      #       journalmatch = "SYSLOG_IDENTIFIER=Nextcloud";
-      #       # END modification to work with syslog instead of logile
-      #       enabled = true;
-      #       port = 443;
-      #       protocol = "tcp";
-      #       filter = "nextcloud";
-      #       maxretry = 3;
-      #       bantime = 86400;
-      #       findtime = 43200;
-      #     };
-      #   };
-      # };
 
-      # environment.etc = {
-      #   # Adapted failregex for syslogs
-      #   "fail2ban/filter.d/nextcloud.local".text = pkgs.lib.mkDefault (pkgs.lib.mkAfter ''
-      #     [Definition]
-      #     failregex = ^.*"remoteAddr":"&lt;HOST&gt;".*"message":"Login failed:
-      #                 ^.*"remoteAddr":"&lt;HOST&gt;".*"message":"Two-factor challenge failed:
-      #                 ^.*"remoteAddr":"&lt;HOST&gt;".*"message":"Trusted domain error.
-      #   '');
-      # };
+      services.fail2ban = {
+        enable = true;
+        # The jail file defines how to handle the failed authentication attempts found by the Nextcloud filter
+        # Ref: https://docs.nextcloud.com/server/latest/admin_manual/installation/harden_server.html#setup-a-filter-and-a-jail-for-nextcloud
+        jails = {
+          nextcloud.settings = {
+            # START modification to work with syslog instead of logile
+            backend = "systemd";
+            journalmatch = "SYSLOG_IDENTIFIER=Nextcloud";
+            # END modification to work with syslog instead of logile
+            enabled = true;
+            port = 443;
+            protocol = "tcp";
+            filter = "nextcloud";
+            maxretry = 3;
+            bantime = 86400;
+            findtime = 43200;
+          };
+        };
+      };
+
+      environment.etc = {
+        # Adapted failregex for syslogs
+        "fail2ban/filter.d/nextcloud.local".text = pkgs.lib.mkDefault (pkgs.lib.mkAfter ''
+          [Definition]
+          failregex = ^.*"remoteAddr":"<HOST>".*"message":"Login failed:
+                      ^.*"remoteAddr":"<HOST>".*"message":"Two-factor challenge failed:
+                      ^.*"remoteAddr":"<HOST>".*"message":"Trusted domain error.
+        '');
+      };
 
       services.nginx.virtualHosts.${"${subdomainName}.${config.modules.server.domain.main}"} = {
         enableACME = true;
